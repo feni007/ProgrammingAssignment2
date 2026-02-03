9982808902822f8c2ee0d07e7891cae1f473c4a4
feni007 <fenidalsaniya7@gmail.com>
2026-02-03 21:42:36 +0530

    Completed Programming Assignment 2

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -1,50 +1,26 @@
-## Function to create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-  
-  inv <- NULL   # yahan inverse store hoga
-  
+  inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  
-  get <- function() {
-    x
-  }
-  
-  setInverse <- function(inverse) {
-    inv <<- inverse
-  }
-  
-  getInverse <- function() {
-    inv
-  }
-  
-  list(
-    set = set,
-    get = get,
-    setInverse = setInverse,
-    getInverse = getInverse
-  )
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-## Function to compute the inverse of the special matrix
 cacheSolve <- function(x, ...) {
-  
-  inv <- x$getInverse()
-  
-  if (!is.null(inv)) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
     message("getting cached inverse")
     return(inv)
   }
-  
   data <- x$get()
-  
-  ### TODO: yahan solve() ka use karke inverse nikalo
-  ### TODO: phir x$setInverse(...) se cache me save karo
-  
-  inv <- solve(data)
-  x$setInverse(inv)
-  
+  inv <- solve(data, ...)
+  x$setinverse(inv)
   inv
 }
